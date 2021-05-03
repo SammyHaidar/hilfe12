ba075735386d281f735ecb727b1b98ad45570848
Sammy <82594873+SammyHaidar@users.noreply.github.com>
2021-05-03 08:15:01 +0200

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,15 @@
+
+public class Product{
+
+    private String shop;
+    private double price;
+
+    public Product test(String shop, double price){
+        this.shop = shop
+        this.price = price;
+    }
+
+    public Product get(){
+        return test;
+    }
+}
